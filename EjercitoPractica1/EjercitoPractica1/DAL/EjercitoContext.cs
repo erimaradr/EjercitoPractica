a4f53c5568bc21f7067c655c3b0c914d4c32fefd
marcosandres.blesa <marcosandres.blesa@gruposese.com>
2019-04-05 15:19:53 +0200

    Instalación paquete Nuget

--- EjercitoPractica1/EjercitoPractica1/DAL/EjercitoContext.cs
+++ EjercitoPractica1/EjercitoPractica1/DAL/EjercitoContext.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using EjercitoPractica1.Models;
 using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
 
 
 namespace EjercitoPractica1.DAL
 {
 
-    public class EjercitoContext : DbContext {
+    public class EjercitoContext: DbContext
+    {
+
+        public EjercitoContext() : base("")
+        {
+        }
+
+        public DbSet<Artilleria> artilleros { get; set; }
+       
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
